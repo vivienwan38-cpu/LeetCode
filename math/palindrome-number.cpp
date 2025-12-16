1e6f049d8749f855f953f2e3676cc374f19e6bca
Vivien <vivienwan38@gmail.com>
2025-12-15 17:15:41 -0800

    LC-palindrome-number [Runtime: 0 ms; Memory: 8.4 MB]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -2,21 +2,49 @@ class Solution {
 public:
     bool isPalindrome(int x) 
     {
-        if (x < 0) 
+        if ( x < 0 || (x % 10 == 0 && x != 0))
             return false;
 
-        long reversed = 0;
-        int original = x;
-
-        while (x > 0)
+        long half_reversed = 0;
+        while (x > half_reversed)
         {
             int digit = x % 10;
-            reversed = reversed * 10 + digit;
+            half_reversed = half_reversed * 10 + digit;
             x = x / 10;
         }
-        return original == reversed;
+
+        return half_reversed == x || half_reversed / 10 == x;
     }
+    /*
+    x       d      hf
+    1234321 1      1
+    123432  2      12
+    12343   3      123
+    1234    4      1234
+    123
+    */
 };
+
+
+// class Solution {
+// public:
+//     bool isPalindrome(int x) 
+//     {
+//         if (x < 0) 
+//             return false;
+
+//         long reversed = 0; //为什么要用long 数字很大吗 感觉不会啊
+//         int original = x;
+
+//         while (x > 0)
+//         {
+//             int digit = x % 10;
+//             reversed = reversed * 10 + digit;
+//             x = x / 10;
+//         }
+//         return original == reversed;
+//     }
+// };
 /*
 x   r   d   r
 121 0   1   1 = 0 + 1
