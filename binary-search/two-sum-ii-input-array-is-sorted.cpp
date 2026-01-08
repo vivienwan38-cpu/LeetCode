cf7c3d32013b16d652556b88abc26e970b379d42
Vivien <vivienwan38@gmail.com>
2026-01-07 16:47:56 -0800

    LC-two-sum-ii-input-array-is-sorted [Runtime: 3 ms; Memory: 19.5 MB]

--- binary-search/two-sum-ii-input-array-is-sorted.cpp
+++ binary-search/two-sum-ii-input-array-is-sorted.cpp
@@ -3,12 +3,13 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) 
     {
         int left = 0, right = numbers.size() - 1;
-        while (true)
+        while (left < right)
         {
             int s = numbers[left] + numbers[right];
             if (s == target)
                 return {left + 1, right + 1};
             s > target ? right-- : left ++;
         }
+        return {};
     }
 };
