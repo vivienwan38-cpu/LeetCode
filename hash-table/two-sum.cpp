431a40a656e3b2a137e15fd127b4fc2e2cb97530
Vivien <vivienwan38@gmail.com>
2026-01-07 18:21:51 -0800

    LC-two-sum [Runtime: 5 ms; Memory: 14.7 MB]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -1,36 +1,14 @@
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) 
-//     {
-//         for (int i = 0; i < nums.size() - 1; i++)
-//         {
-//             for (int j = i + 1; j < nums.size(); j++)
-//             {
-//                 if (target == nums[i] + nums[j])
-//                     return {i, j};
-//             }
-//         }
-//         return {};
-//     }
-// };
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-        unordered_map<int, int> hash;
-        for (int i = 0; i < nums.size(); i++)
+        unordered_map<int,int> hash;
+        for (int i = 0; ; i++)
         {
-            hash[nums[i]] = i; //hash返回索引
+            auto it = hash.find(target - nums[i]);
+            if (it != hash.end())
+                return {it->second, i};
+            hash[nums[i]] = i;
         }
-
-        for (int j = 0; j < nums.size(); j++)
-        {
-            int n = target - nums[j];
-
-            if (hash.find(n) != hash.end() && hash[n] != j)
-                return {j, hash[n]};
-        }
-        return {};
     }
 };
