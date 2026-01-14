fd27bce23a0c9bc49c25075c79a7f4c1f09cbeab
Vivien <vivienwan38@gmail.com>
2026-01-13 17:10:55 -0800

    LC-number-of-good-pairs [Runtime: 0 ms; Memory: 9.8 MB]

--- counting/number-of-good-pairs.cpp
+++ counting/number-of-good-pairs.cpp
@@ -5,10 +5,9 @@ public:
         int i, j;
         int counter = 0;
         for (i = 0; i < nums.size() - 1; i++)
-            for (j = 1; j < nums.size(); j++)
-                if (i < j)
-                    if (nums[i] == nums[j])
-                        counter++;
+            for (j = i + 1; j < nums.size(); j++)
+                if (nums[i] == nums[j])
+                    counter++;
         return counter;
     }
 };
