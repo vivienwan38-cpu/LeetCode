5d42231c0ccee665acc8e3503b5445ae91414937
Vivien <vivienwan38@gmail.com>
2026-01-14 16:55:07 -0800

    LC-maximum-difference-between-increasing-elements [Runtime: 0 ms; Memory: 11.7 MB]

--- array/maximum-difference-between-increasing-elements.cpp
+++ array/maximum-difference-between-increasing-elements.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
-        int ans;
+        int ans = -1;
         int min_num = nums[0];
         for (auto n : nums){
             ans = max(ans, n - min_num);
             min_num = min(min_num, n);
         }
         if (ans == 0) return -1;
-        return ans;
+        else return ans;
     }
 };
