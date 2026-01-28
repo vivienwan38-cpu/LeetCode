0202cfa6f37de8f3de4d7bc7e35b329b514f91d0
Vivien <vivienwan38@gmail.com>
2026-01-28 05:56:03 -0800

    LC-largest-positive-integer-that-exists-with-its-negative [Runtime: 0 ms; Memory: 23.6 MB]

--- hash-table/largest-positive-integer-that-exists-with-its-negative.cpp
+++ hash-table/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    int findMaxK(vector<int>& nums) 
-    {
-        sort(nums.begin(), nums.end());
-        int i = 0;
-        int j = nums.size() - 1;
-        while (j > 0 && nums[j] > 0) //j要注意边界 & 找正数
+    int findMaxK(vector<int>& nums) {
+        ranges::sort(nums);
+        for (int i = 0, j = nums.size() - 1; j > i; j--)
         {
             while (i < j && nums[i] < -nums[j])
                 i++;
             if (nums[i] == -nums[j])
-                return abs(nums[i]);
-            j--;
+                return nums[j];
         }
         return -1;
     }
 };
-// nums = [-2, -1, 0, 2, 3] //跳过i++直接j--
-// nums = [-3, -1, 0, 2, 3] //直接第二个if返回ans
-// nums = [-7, -1, 0, 1, 6] //i++然后j--然后找到了
