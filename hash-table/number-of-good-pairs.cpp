7608e88da82ed2028d9838d85ac816673cfdef73
Vivien <vivienwan38@gmail.com>
2026-01-28 02:27:10 -0800

    LC-number-of-good-pairs [Runtime: 0 ms; Memory: 9.7 MB]

--- hash-table/number-of-good-pairs.cpp
+++ hash-table/number-of-good-pairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int numIdenticalPairs(vector<int>& nums) 
-    {
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int> hash;
         int counter = 0;
-        unordered_map<int,int>hash;
-        for (auto n : nums)
-        {
-            counter += hash[n];
-            hash[n]++;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] == nums[j])
+                    counter++;
+            }
         }
         return counter;
     }
